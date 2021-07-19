01925a56265873527a602a666b5a2908be260e80
Aravind Kumar <aravind.kumar@diatoz.com>
2021-07-19 16:12:22 +0530

    index creation added

--- database.py
+++ database.py
@@ -1,10 +1,26 @@
 import motor.motor_asyncio
 import configparser
+from pymongo import MongoClient
 
 config = configparser.ConfigParser()
 config.read('.env')
 
 MONGODB_URL = config["default"]["DB_URL"]
+MONGODB_NAME = config["default"]["DB_NAME"]
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
-db = client.recommenders
+db = client[MONGODB_NAME]
+
+# for creating index 
+# this will only work first time when you are creating collections
+# can not create index after adding data to collection
+pymongo_client = MongoClient(MONGODB_URL)
+pymongo_db = pymongo_client[MONGODB_NAME]
+
+collection = pymongo_db['jobs']
+collection.create_index("job_id", unique=True)
+
+collection = pymongo_db['resumes']
+collection.create_index("candidate_id", unique=True)
+pymongo_client.close()
+
